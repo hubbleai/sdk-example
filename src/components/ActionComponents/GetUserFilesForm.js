9b78590d6af18927eb25f869217ae2ec3cb544fb
Kailash Anantha <kailash@polymer.fyi>
2023-11-01 22:11:13 +0530

    Removing an unnecessary field from the user files form

--- src/components/ActionComponents/GetUserFilesForm.js
+++ src/components/ActionComponents/GetUserFilesForm.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import * as Carbon from 'carbon-connect-js';
 
 const GetUserFilesForm = ({ setJsonResponse, setIsLoading, accessToken }) => {
-  const [fileId, setFileId] = useState('');
-
   const handleSubmit = (event) => {
     event.preventDefault();
     setIsLoading(true);
@@ -18,13 +16,6 @@ const GetUserFilesForm = ({ setJsonResponse, setIsLoading, accessToken }) => {
 
   return (
     <div className="flex justify-center w-full items-center h-full my-6 flex-col">
-      <input
-        type="text"
-        value={fileId}
-        onChange={(e) => setFileId(e.target.value)}
-        placeholder="Enter file ID"
-        className="w-64 px-4 py-2 border rounded-lg text-black"
-      />
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline h-10 mt-4"
         type="button"
